150f8719b76dda209ca5c30d7f439df35b9ac9dc
enRamirez <ricky.ramirez125042@gmail.com>
2023-09-18 20:37:23 -0500

    added a toggle switch

--- src/Body/Body.js
+++ src/Body/Body.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import BodyPart from './BodyPart/BodyPart';
+import './Body.css';
 
 const Body = () => {
+
+  const [selectedOption, setSelectedOption] = useState('Stretches');
+
+  const toggleOption = () => {
+    setSelectedOption(selectedOption === 'Stretches' ? 'Muscle Strength' : 'Stretches');
+  }
+
   return (
     <div>
-      <h1>2D Body</h1>
+      <h1>Select Your Body Focus</h1>
+
+      <div className='toggle-container'>
+        <button className='workoutToggle' onClick={toggleOption}>
+          Toggle: {selectedOption}
+        </button>
+      </div>
+
       <div className="body-container">
-        <BodyPart partName="Head" />
-        <BodyPart partName="Torso" />
-        <BodyPart partName="Legs" />
-        <BodyPart partName="Arms" />
+        <BodyPart partName={`${selectedOption} - Head`} />
+        <BodyPart partName={`${selectedOption} - Torso`} />
+        <BodyPart partName={`${selectedOption} - Legs`} />
+        <BodyPart partName={`${selectedOption} = Arms`} />
       </div>
     </div>
   );
